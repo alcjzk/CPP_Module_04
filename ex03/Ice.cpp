f01280559dd9417ec7be2bee436c4bd8f67d1e25
Tuomas <tuomas.jaasalo@alceon.fi>
2023-11-07 18:45:14 +0200

    Impl Ice

---
+++ ex03/Ice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Ice.hpp"
+
+Ice::Ice() : AMateria("Ice") {}
+
+Ice::Ice(const Ice& other) : AMateria(other) {}
+
+Ice::~Ice() {}
+
+Ice& Ice::operator=(const Ice& other)
+{
+    if (this == &other)
+        return *this;
+    AMateria::operator=(other);
+    return *this;
+}
+
+AMateria* Ice::clone() const
+{
+    return new Ice(*this);
+}
+
+void Ice::use(ICharacter& target)
+{
+    std::cout << "* shoots an ice bolt at " << target.getName() << " *\n";
+}
---
+++ ex03/Ice.hpp
@@ -0,0 +1,19 @@
+#ifndef ICE_H
+# define ICE_H
+
+# include "AMateria.hpp"
+
+class Ice : AMateria
+{
+    public:
+        Ice();
+        Ice(const Ice& other);
+        ~Ice();
+
+        Ice&    operator=(const Ice& other);
+
+        virtual AMateria*   clone() const;
+        virtual void        use(ICharacter& target);
+};
+
+#endif
